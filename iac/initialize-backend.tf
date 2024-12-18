3925b01fb3b9adb7e8b4019af46aa67078b8461c
chadgarlandscg <chadgarlandscg@gmail.com>
2024-12-18 00:56:39 -0700

    Add HCL edit workflows

---
+++ iac/initialize-backend.tf
@@ -0,0 +1,33 @@
+resource "aws_s3_bucket" "tf_backend" {
+    bucket = "${var.organization_name}-terraform-state"
+    versioning {
+        enabled = true
+    }
+    server_side_encryption_configuration {
+        rule {
+            apply_server_side_encryption_by_default {
+                sse_algorithm = "AES256"
+            }
+        }
+    }
+    object_lock_configuration {
+        object_lock_enabled = "Enabled"
+    }
+    tags = {
+        Name = "S3 Remote Terraform State Store"
+    }
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+    name           = "${var.organization_name}-terraform-lock"
+    read_capacity  = 5
+    write_capacity = 5
+    hash_key       = "LockID"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+    tags = {
+        "Name" = "DynamoDB Terraform State Lock Table"
+    }
+}
