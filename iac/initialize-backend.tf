8f352e122d54d7556c298e04483fd1afcf0a4980
chadgarlandscg <chadgarlandscg@gmail.com>
2024-12-20 21:00:21 -0700

    Add secrets before terraform apply in workflow

--- iac/initialize-backend.tf
+++ iac/initialize-backend.tf
@@ -11,30 +11,22 @@ terraform {
 
 resource "aws_s3_bucket" "tf_backend" {
     bucket = "${var.organization_name}-terraform-state"
+    versioning {
+        enabled = true
+    }
+    server_side_encryption_configuration {
+        rule {
+            apply_server_side_encryption_by_default {
+                sse_algorithm = "AES256"
+            }
+        }
+    }
+    object_lock_configuration {
+        object_lock_enabled = "Enabled"
+    }
     tags = {
         Name = "S3 Remote Terraform State Store"
     }
-}
-
-resource "aws_s3_bucket_versioning" "tf_backend_versioning" {
-    bucket = aws_s3_bucket.tf_backend
-    versioning_configuration {
-        status = "Enabled"
-    }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "tf_backend_encryption" {
-    bucket = aws_s3_bucket.tf_backend
-    rule {
-        apply_server_side_encryption_by_default {
-            sse_algorithm = "AES256"
-        }
-    }
-}
-
-resource "aws_s3_bucket_object_lock_configuration" "tf_backend_lock" {
-    bucket = aws_s3_bucket.tf_backend
-    object_lock_enabled = "Enabled"
 }
 
 resource "aws_dynamodb_table" "tf_lock" {
